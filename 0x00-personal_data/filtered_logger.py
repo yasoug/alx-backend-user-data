6190b35976b9d804d9cad7eab0640f417fb55315
yasoug <ougouisyassine@gmail.com>
2024-02-08 18:56:50 +0000

    task 0 : commit 1

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""module for the function filter_datum"""
+
+import re
+from typing import List
+
+
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """returns the log message obfuscated"""
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
