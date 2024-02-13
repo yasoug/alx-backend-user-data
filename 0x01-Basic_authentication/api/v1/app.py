46e81b7eb8b94b710a8ef90cae6559dae6f9aa5d
yasoug <ougouisyassine@gmail.com>
2024-02-13 20:30:18 +0000

    task 6 : commit 1

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -17,6 +17,9 @@ auth = None
 if getenv('AUTH_TYPE') == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
+elif getenv('AUTH_TYPE') == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.before_request
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""module for the class BasicAuth"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """class for basic authentication"""
+    pass
