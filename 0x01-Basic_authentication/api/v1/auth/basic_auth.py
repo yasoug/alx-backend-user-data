fb7dda4bb80cf9e198d7e262c95bc5134af110bd
yasoug <ougouisyassine@gmail.com>
2024-02-13 20:48:31 +0000

    task 7 : commit 1

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,4 +6,12 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """class for basic authentication"""
-    pass
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str
+            ) -> str:
+        """Returns the Base64 part of the request header"""
+        if not (authorization_header and isinstance(authorization_header, str)
+                and authorization_header.startswith('Basic ')):
+            return None
+        return authorization_header[6:]
