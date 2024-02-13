30799a9e7e2d1faa1a67e5d1d03a4d8daf1d4acf
yasoug <ougouisyassine@gmail.com>
2024-02-13 22:13:00 +0000

    task 12 : commit 1

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -42,7 +42,9 @@ class BasicAuth(Auth):
                 isinstance(decoded_base64_authorization_header, str) and
                 ':' in decoded_base64_authorization_header):
             return None, None
-        email, password = decoded_base64_authorization_header.split(':')
+        line = decoded_base64_authorization_header.split(':')
+        email = line[0]
+        password = ':'.join(line[1:])
         return email, password
 
     def user_object_from_credentials(
