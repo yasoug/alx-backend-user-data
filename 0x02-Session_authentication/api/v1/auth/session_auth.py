c14ca1414ccb6a9fde19e60d3b623011e49c726c
yasoug <ougouisyassine@gmail.com>
2024-02-14 20:42:14 +0000

    task 1 : commit 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""module for the class SessionAuth"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """class for session authentication"""
+    pass
