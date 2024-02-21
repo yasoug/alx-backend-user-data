f50dd0fc30c87de90680f9f77c76abe228817d74
yasoug <ougouisyassine@gmail.com>
2024-02-21 21:47:20 +0000

    task 6 : commit 1

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""basic flask app"""
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def msg():
+    """single GET route /"""
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
