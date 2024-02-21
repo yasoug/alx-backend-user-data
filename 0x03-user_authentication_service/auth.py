4f7af827bfac534f2dedafa70b12902c3fa19dd2
yasoug <ougouisyassine@gmail.com>
2024-02-21 22:03:23 +0000

    task 8 : commit 1

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -28,3 +28,11 @@ class Auth:
             return self._db.add_user(email, _hash_password(password))
         else:
             raise ValueError(f"User {email} already exists")
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Checks credentials validation"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
