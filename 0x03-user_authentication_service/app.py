b1e962857dd0902d46c272562a06e97823b7aafc
yasoug <ougouisyassine@gmail.com>
2024-02-22 13:17:01 +0000

    task 15 : commit 1

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -48,5 +48,15 @@ def logout():
     return redirect("/")
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile():
+    """Returns user profile"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    return jsonify({"email": user.email}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
