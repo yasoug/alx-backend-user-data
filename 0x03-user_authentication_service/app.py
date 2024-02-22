ccfc4803b09e73f34aca59722905421be929e4fb
yasoug <ougouisyassine@gmail.com>
2024-02-22 12:37:51 +0000

    task 11 : commit 1

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """basic flask app"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -23,6 +23,18 @@ def users():
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
     return jsonify({"email": email, "message": "user created"})
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """Function to respond to the POST /sessions route"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", AUTH.create_session(email))
+    return response
 
 
 if __name__ == "__main__":
