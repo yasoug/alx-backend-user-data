68acab8dccc7703d0e31b4eec28e4ebbaf6af455
yasoug <ougouisyassine@gmail.com>
2024-02-22 13:20:50 +0000

    task 16 : commit 1

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -69,3 +69,13 @@ class Auth:
         if not user_id:
             return None
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generates a reset password token for the user"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError()
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
