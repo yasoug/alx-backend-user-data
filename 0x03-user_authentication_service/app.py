f63d52a8f9a3235d2fae7ec51db5598219ddd10b
yasoug <ougouisyassine@gmail.com>
2024-02-22 12:55:56 +0000

    task 14 : commit 1

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """basic flask app"""
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 app = Flask(__name__)
@@ -35,6 +35,17 @@ def login():
     response = jsonify({"email": email, "message": "logged in"})
     response.set_cookie("session_id", AUTH.create_session(email))
     return response
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """destroys the session and redirect the user to GET /"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect("/")
 
 
 if __name__ == "__main__":
